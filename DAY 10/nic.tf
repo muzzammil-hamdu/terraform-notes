098d1fde85d9bb2dd7da6f5990f8d47da3c3f74c
muzzammil-hamdu <shaikhussain752@gmail.com>
2025-07-30 12:40:10 +0530

    Create NIC

---
+++ DAY 10/nic.tf
@@ -0,0 +1,16 @@
+variable "nic" {
+ default = "nicname"
+}
+variable "ipconfigname" {
+ default = "ipname"
+}
+resource "azurerm_network_interface" "example" {
+ name = var.nic
+ location = azurerm_resource_group.example.location
+ resource_group_name = azurerm_resource_group.example.name
+ ip_configuration {
+ name = var.ipconfigname
+ subnet_id = azurerm_subnet.example.id
+ private_ip_address_allocation = "Dynamic"
+ }
+}
